72450514f308ce6c1015d24f7bb1a758518d06b5
hayato-yamashita <h-yamashita@tes.co.jp>
2018-12-20 18:03:23 +0900

    v-menu はかぶっていた

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   v-app
-    v-menu
+    v-bottom-menu
     v-toolbar(app dark flat color="primary")
       nuxt-link.d-flex(to="/")
         img(src="/logo.svg" alt="logo" height="38px" width="38px" aria-hidden="true")
@@ -17,13 +17,13 @@
 </template>
 
 <script>
-import Menu from '~/components/Menu'
+import BottomMenu from '~/components/BottomMenu'
 import ScrollToTop from '~/components/ScrollToTop'
 
 export default {
   components: {
     'v-scroll-to-top': ScrollToTop,
-    'v-menu': Menu
+    'v-bottom-menu': BottomMenu
   }
 }
 </script>
