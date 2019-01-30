2dba35b9548635deace3132d0469a7eb3e14e44b
hayato-yamashita <h-yamashita@tes.co.jp>
2019-01-30 16:35:53 +0900

    yarn upgrade (2019-01-30).

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -11,10 +11,6 @@ const config = {
 }
 if (!firebase.apps.length) {
   firebase.initializeApp(config)
-
-  const firestore = firebase.firestore()
-  const settings = { timestampsInSnapshots: true }
-  firestore.settings(settings)
 }
 
 export default firebase
